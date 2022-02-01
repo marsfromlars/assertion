52a7f1d9867e8f175d178152258a1709fc507dfd
Lars Löffler <lars.loeffler@infodesire.com>
2022-02-01 15:47:26 +0100

    Add assetion assertError and assertNoError, beautify list a little

--- assertion.js
+++ assertion.js
@@ -1,8 +1,9 @@
 class Result {
-    constructor( expression, message, result, details ) {
+    constructor( expression, message, result, details, assertionType ) {
+        this.result = result
+        this.assertionType = assertionType
         this.expression = expression
         this.message = message
-        this.result = result
         this.details = details
     }
 }
@@ -22,31 +23,112 @@ function getResults() {
 }
 
 function assert( expression, message ) {
+    assertTrue( expression, message )
+}
 
-    assertEquals( 'true', expression, message )
+function assertTrue( expression, message ) {
+    assertTrueOrFalse( expression, message, true )
+}
+
+function assertFalse( expression, message ) {
+    assertTrueOrFalse( expression, message, false )
+}
+
+function assertTrueOrFalse( expression, message, trueOrFalse ) {
+
+    let result = 'OK'
+    let assertionType = trueOrFalse ? 'True' : 'False'
+
+    try {
+
+        let evalResult = eval( expression )
+        let details = ''
+        if( trueOrFalse != evalResult ) {
+            details = 'Expected: ' + expression + ' is ' + trueOrFalse + ' but was ' + evalResult
+            result = 'Failed'
+        }
+        getResults().add( new Result( expression, message, result, details, assertionType ) )
+    
+    }
+    catch( ex ) {
+
+        getResults().add( new Result( expression, message, 'Error', '' + ex, assertionType ) )
+
+    }
+
+}
+
+function assertError( expression, message ) {
+    assertErrorOrNot( expression, message, true )
+}
+
+function assertNoError( expression, message ) {
+    assertErrorOrNot( expression, message, false )
+}
+
+function assertErrorOrNot( expression, message, errorOrNot ) {
+
+    let result = 'OK'
+    let assertionType = errorOrNot ? 'Error' : 'NoError'
+    let details = ''
+
+    try {
+
+        let evalResult = eval( expression )
+        if( errorOrNot ) {
+            details = 'Expected: error but no error occurred while evaluating ' + expression
+            result = 'Failed'
+        }
+        getResults().add( new Result( expression, message, result, details, assertionType ) )
+    
+    }
+    catch( ex ) {
+
+        if( !errorOrNot ) {
+            details = 'Expected: no error but error occurred while evaluating ' + expression + ', error was: ' + ex
+            result = 'Failed'
+        }
+        getResults().add( new Result( expression, message, result, details, assertionType ) )
+
+    }
 
 }
 
 function assertEquals( expected, actual, message ) {
+    assertEqualsOrNot( expected, actual, message, true )
+}
 
-    let expression = actual + ' == ' + expected
+function assertNotEquals( expected, actual, message ) {
+    assertEqualsOrNot( expected, actual, message, false )
+}
+
+function assertEqualsOrNot( expected, actual, message, equalsOrNot ) {
+        
+    let expression = actual + ( equalsOrNot ? ' == ' : ' != ' ) + expected
     let result = 'OK'
+    let assertionType = equalsOrNot ? 'Equals' : 'NotEquals'
 
     try {
 
         let expectedResult = eval( expected )
         let actualResult = eval( actual )
         let details = ''
-        if( actualResult != expectedResult ) {
-            details = 'Expected: ' + expectedResult + ' but was ' + actualResult
+        let evalResult = actualResult == expectedResult
+        if( evalResult != equalsOrNot ) {
+            if( equalsOrNot ) {
+                details = 'Expected: ' + expectedResult + ' but was ' + actualResult
+            }
+            else {
+                details = 'Expected: ' + expectedResult + ' to be different from ' + actual
+            }
             result = 'Failed'
         }
-        getResults().add( new Result( expression, message, result ) )
+        getResults().add( new Result( expression, message, result, details, assertionType ) )
     
     }
     catch( ex ) {
 
-        getResults().add( new Result( expression, message, 'Error', '' + ex ) )
+        getResults().add( new Result( expression, message, 'Error', '' + ex, assertionType ) )
 
     }
     
@@ -66,6 +148,9 @@ function printResults( targetEl ) {
     td.innerHTML = 'Result'
     tr.appendChild( td )
     td = document.createElement( 'th' )
+    td.innerHTML = 'Assertion'
+    tr.appendChild( td )
+    td = document.createElement( 'th' )
     td.innerHTML = 'Expression'
     tr.appendChild( td )
     td = document.createElement( 'th' )
@@ -81,6 +166,9 @@ function printResults( targetEl ) {
         table.appendChild( tr )
         let td = document.createElement( 'td' )
         td.innerHTML = result.result
+        tr.appendChild( td )
+        td = document.createElement( 'td' )
+        td.innerHTML = 'assert' + result.assertionType
         tr.appendChild( td )
         td = document.createElement( 'td' )
         td.innerHTML = result.expression ? result.expression : '-'
